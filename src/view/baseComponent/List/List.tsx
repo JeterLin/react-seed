bc3f6bdee9914486c4b3259e2331608ae14a5bb7
linjunteng <linjunteng1@gemdale.com>
2020-05-09 16:08:36 +0800

    调整基础组件导出

---
+++ src/view/baseComponent/List/List.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { List as AntdList } from 'antd';
+import { ListProps } from 'antd/lib/list';
+import { DefaultEmptyList } from './DefaultEmptyList';
+
+type ExtPropTypes = Partial<{
+    emptyListPlaceholder: JSX.Element | null | boolean;
+}>;
+
+export function List<ItemType extends object, PropTypes extends ListProps<ItemType> & ExtPropTypes>(props: PropTypes): JSX.Element | null {
+    const { emptyListPlaceholder, ...listProps } = props;
+    if (emptyListPlaceholder) {
+        if (Array.isArray(props.dataSource) && props.dataSource.length > 0) {
+            return <AntdList {...listProps} />;
+        }
+        if (typeof emptyListPlaceholder === 'boolean') {
+            return <DefaultEmptyList />;
+        }
+        return emptyListPlaceholder;
+    }
+    return <AntdList {...props} />;
+}
+
+export const ListItem = AntdList.Item;
--- src/view/baseComponent/index.js
+++ src/view/baseComponent/index.js
@@ -1,3 +1,5 @@
 export {Table} from './Table';
 export {Input} from './Input';
 export {Checkbox} from './Checkbox';
+export {Button} from './Button';
+export {List, ListItem} from './List/List';
