1980eae190ab093f7661b9cc2f227cbf0636e17d
JLin <jeterlin2012@gmail.com>
2020-05-17 22:39:01 +0800

    调整ServiceType, AxioServiceType等类型，用于实现todoService对象在调用方法时可以推断到相应的函数定义

--- src/service/types.d.ts
+++ src/service/types.d.ts
@@ -2,14 +2,20 @@ import { AxiosResponse } from 'axios';
 export interface BaseRespType {
     code: number;
     data: any;
+    errMsg?: string;
 }
-export type ServiceHandlerType = (url: string, payload: object) => Promise<BaseRespType>;
-export type ServiceType = {
-    [FuncName: string]: ServiceHandlerType;
+export type ServiceHandlerType = (payload?: object) => Promise<BaseRespType>;
+
+export type IBaseService<T extends object = object> = {
+    [K in keyof T]: Function;
+};
+export type ServiceType<T extends object> = {
+    [K in keyof IBaseService<T>]: T[K] extends ServiceHandlerType ? T[K] : never;
+};
+export type AxiosServiceType<T extends object> = {
+    [K in keyof IBaseService<T>]: T[K] extends (payload?: object) => Promise<AxiosResponse<BaseRespType>> ? T[K] : never;
 };
 
-export type AxiosServiceType = {
-    [FuncName: string]: (url: string, payload: object) => Promise<AxiosResponse<BaseRespType>>;
+export type ToServiceType<T extends object> = {
+    [K in keyof AxiosServiceType<T>]: T[K] extends AxiosServiceType<T>[K] ? ServiceHandlerType : never;
 };
-
-export type ExceptionHandlerType = (err: any, url: string, payload: object) => void;
