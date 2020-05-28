2154922614803973df706a0f1eecc05f20f47719
JLin <jeterlin2012@gmail.com>
2020-05-29 00:07:16 +0800

    添加样式文件

--- src/view/demo/todoDetail/TodoDetail.tsx
+++ src/view/demo/todoDetail/TodoDetail.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+
+import ss from './TodoDetail.less';
+
 function TodoDetail() {
-    return null;
+    return (
+        <div className={ss.wrapper}>
+            <header className={ss.header}></header>
+        </div>
+    );
 }
 
-export {TodoDetail as default};
+export { TodoDetail as default };
