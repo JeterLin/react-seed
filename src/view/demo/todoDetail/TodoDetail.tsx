d64f8738f8e3e0f34df0f8ae0e54d88b83fb9b5f
JLin <jeterlin2012@gmail.com>
2020-05-31 21:56:50 +0800

    添加输入表单

--- src/view/demo/todoDetail/TodoDetail.tsx
+++ src/view/demo/todoDetail/TodoDetail.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import {IRouteChildrenProps} from '@router';
-import {ArrowLeft} from '@view/baseComponent';
+import React, { FC } from 'react';
+import { IRouteChildrenProps } from '@router';
+import { ArrowLeft, Form, FormItem, Input, TextArea } from '@view/baseComponent';
 
 import ss from './TodoDetail.less';
-import {DetailTitle} from './DetailTitle';
+import { DetailTitle } from './DetailTitle';
 
 type IProps = IRouteChildrenProps;
+const FormTitle: FC<{ title: string }> = (props) => {
+    return <span className={ss.title}>{props.title}</span>;
+};
 function TodoDetail(props: IProps) {
+    const labelCol = { span: 6 };
+    const wrapperCol = { span: 14 };
+
     return (
         <div className={ss.wrapper}>
             <header className={ss.header}>
-                <ArrowLeft/>
-                <DetailTitle/>
+                <ArrowLeft />
+                <DetailTitle />
             </header>
+            <div className={ss.content}>
+                <Form name="detailForm" labelCol={labelCol} wrapperCol={wrapperCol}>
+                    <FormItem className={ss.formItemVerticalGap} label={<FormTitle title="title" />} required>
+                        <Input placeholder="add todo title" size="large" className={ss.control} />
+                    </FormItem>
+                    <FormItem label={<FormTitle title="description" />}>
+                        <TextArea placeholder="add todo description" className={ss.control} rows={6}/>
+                    </FormItem>
+                    <FormItem label={<FormTitle title="comment" />}>
+                        <TextArea placeholder="add todo comment" className={ss.control} rows={6}/>
+                    </FormItem>
+                </Form>
+            </div>
         </div>
     );
 }
