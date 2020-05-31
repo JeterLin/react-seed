f98c73c4122d8d8108a83a7e4cb56be86dd5218c
JLin <jeterlin2012@gmail.com>
2020-05-31 20:47:46 +0800

    添加todo detail基本样式和头部组件

---
+++ src/view/demo/todoDetail/DetailTitle.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useCallback, memo } from 'react';
+import { Dropdown, Ellipsis, Menu, ItemConfig } from '@view/baseComponent';
+
+import ss from './DetailTitle.less';
+
+const MenuItemsConfig: ItemConfig[] = [
+    { title: 'Detail', key: 'todo-detail' },
+    { title: 'Log', key: 'todo-log' },
+];
+
+interface IProps {
+    onTitleChange?: (nextTitle: string) => void;
+}
+
+export function DetailTitle(props: IProps): JSX.Element {
+    const [title, setTitle] = useState<string>(MenuItemsConfig[0].title);
+    const handleTitleChange = useCallback((params) => {
+        setTitle(params.itemConfig.title);
+    }, []);
+    const menu = <Menu menuItems={MenuItemsConfig} onClick={handleTitleChange} />;
+    return (
+        <div className={ss.wrapper}>
+            <span>{title}</span>
+            <Dropdown placement="bottomCenter" overlay={menu}>
+                <Ellipsis />
+            </Dropdown>
+        </div>
+    );
+}
--- src/view/demo/todoDetail/TodoDetail.tsx
+++ src/view/demo/todoDetail/TodoDetail.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {IRouteChildrenProps} from '@router';
+import {ArrowLeft} from '@view/baseComponent';
 
 import ss from './TodoDetail.less';
+import {DetailTitle} from './DetailTitle';
 
 type IProps = IRouteChildrenProps;
 function TodoDetail(props: IProps) {
     return (
         <div className={ss.wrapper}>
-            <header className={ss.header}></header>
+            <header className={ss.header}>
+                <ArrowLeft/>
+                <DetailTitle/>
+            </header>
         </div>
     );
 }
