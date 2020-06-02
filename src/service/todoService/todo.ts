680bbca06c4f72e8aa837fca18d230ee64115f1c
linjunteng <linjunteng1@gemdale.com>
2020-06-02 10:37:53 +0800

    删除wrapRespException方法

--- src/service/todoService/todo.ts
+++ src/service/todoService/todo.ts
@@ -1,16 +1,14 @@
 // import axios from 'axios';
 import { BaseRespType } from '../types';
-import { axios, toService, wrapRespException, makeUrl } from '../_utils';
-export const todoService = wrapRespException(
-    toService({
-        addTodo(payload) {
-            return axios.post<BaseRespType>(makeUrl('todo/add'), payload);
-        },
-        listTodo() {
-            return axios.get<BaseRespType>(makeUrl('todo/list'));
-        },
-        deleteTodo(payload) {
-            return axios.delete<BaseRespType>(makeUrl('todo/delete'), { params: payload });
-        },
-    })
-);
+import { axios, toService, makeUrl } from '../_utils';
+export const todoService = toService({
+    addTodo(payload) {
+        return axios.post<BaseRespType>(makeUrl('todo/add'), payload);
+    },
+    listTodo() {
+        return axios.get<BaseRespType>(makeUrl('todo/list'));
+    },
+    deleteTodo(payload) {
+        return axios.delete<BaseRespType>(makeUrl('todo/delete'), { params: payload });
+    },
+});
