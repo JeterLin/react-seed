53de52f4b7d45d05f8f1a57c86e5b14198e8631a
linjunteng <linjunteng1@gemdale.com>
2020-06-22 11:55:31 +0800

    将any替换为unknown

--- src/mw/logger.ts
+++ src/mw/logger.ts
@@ -1,6 +1,5 @@
 import { Middleware, PayloadAction, Dispatch } from '@reduxjs/toolkit';
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-type IAction = PayloadAction<any>;
+type IAction = PayloadAction<unknown>;
 export const logger: Middleware = () => (nextDispatch: Dispatch<IAction>) => (action: IAction): IAction => {
     if (process.env.NODE_ENV === 'development' && action.type) {
         console.group(action.type);
