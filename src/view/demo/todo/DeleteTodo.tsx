58f2789184db0a5e9d76071f8d4141b1b0c1d7c0
linjunteng <linjunteng1@gemdale.com>
2020-06-24 09:44:02 +0800

    重构编辑详情和删除todo按钮

--- src/view/demo/todo/DeleteTodo.tsx
+++ src/view/demo/todo/DeleteTodo.tsx
@@ -1,15 +1,14 @@
 import React, { useCallback } from 'react';
-import { Button } from '@view/baseComponent';
 import { TodoItemType } from '@store/todoList';
-
+import { Delete } from '@view/baseComponent/Icons';
 import ss from './DeleteTodo.less';
 
 type IProps = Partial<{ onDelete: (item: TodoItemType | undefined) => void; item: TodoItemType }>;
 export function DeleteTodo(props: IProps): JSX.Element | null {
     const handleClick = useCallback(() => props.onDelete && props.onDelete(props.item), [props.item]);
     return (
-        <Button onClick={handleClick} className={ss.delBtn} danger type="primary">
-            Delete
-        </Button>
+        <div className={ss.delBtn} onClick={handleClick}>
+            <Delete className={ss.icon}/>
+        </div>
     );
 }
